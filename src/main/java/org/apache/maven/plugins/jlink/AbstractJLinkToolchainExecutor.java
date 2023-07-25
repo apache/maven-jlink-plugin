5e4d88b43cff73288015ad15d0ac15d842c35e66
Benjamin Marwell <bmarwell@apache.org>
2023-07-25 21:23:43 +0200

    [MJLINK-71] require maven 3.6.3 (#165)

--- src/main/java/org/apache/maven/plugins/jlink/AbstractJLinkToolchainExecutor.java
+++ src/main/java/org/apache/maven/plugins/jlink/AbstractJLinkToolchainExecutor.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.jlink;
 
 /*
@@ -19,6 +37,11 @@ package org.apache.maven.plugins.jlink;
  * under the License.
  */
 
+import java.io.File;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -28,24 +51,16 @@ import org.apache.maven.shared.utils.cli.CommandLineUtils;
 import org.apache.maven.shared.utils.cli.Commandline;
 import org.apache.maven.toolchain.Toolchain;
 
-import java.io.File;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-abstract class AbstractJLinkToolchainExecutor extends AbstractJLinkExecutor
-{
+abstract class AbstractJLinkToolchainExecutor extends AbstractJLinkExecutor {
     private final Toolchain toolchain;
 
-    AbstractJLinkToolchainExecutor( Toolchain toolchain, Log log )
-    {
-        super( log );
+    AbstractJLinkToolchainExecutor(Toolchain toolchain, Log log) {
+        super(log);
         this.toolchain = toolchain;
     }
 
-    protected Optional<Toolchain> getToolchain()
-    {
-        return Optional.ofNullable( this.toolchain );
+    protected Optional<Toolchain> getToolchain() {
+        return Optional.ofNullable(this.toolchain);
     }
 
     /**
@@ -54,151 +69,128 @@ abstract class AbstractJLinkToolchainExecutor extends AbstractJLinkExecutor
      * @return the exit code ({@code 0} on success).
      */
     @Override
-    public int executeJlink( List<String> jlinkArgs ) throws MojoExecutionException
-    {
+    public int executeJlink(List<String> jlinkArgs) throws MojoExecutionException {
         File jlinkExecutable = getJlinkExecutable();
-        getLog().info( "Toolchain in maven-jlink-plugin: jlink [ " + jlinkExecutable + " ]" );
+        getLog().info("Toolchain in maven-jlink-plugin: jlink [ " + jlinkExecutable + " ]");
 
-        Commandline cmd = createJLinkCommandLine( jlinkArgs );
-        cmd.setExecutable( jlinkExecutable.getAbsolutePath() );
+        Commandline cmd = createJLinkCommandLine(jlinkArgs);
+        cmd.setExecutable(jlinkExecutable.getAbsolutePath());
 
-        return executeCommand( cmd );
+        return executeCommand(cmd);
     }
 
-    private File getJlinkExecutable()
-    {
-        return new File( getJLinkExecutable() );
+    private File getJlinkExecutable() {
+        return new File(getJLinkExecutable());
     }
 
     @Override
-    public Optional<File> getJmodsFolder( /* nullable */ File sourceJdkModules )
-    {
+    public Optional<File> getJmodsFolder(/* nullable */ File sourceJdkModules) {
         // Really Hacky...do we have a better solution to find the jmods directory of the JDK?
         File jLinkParent = getJlinkExecutable().getParentFile().getParentFile();
         File jmodsFolder;
-        if ( sourceJdkModules != null && sourceJdkModules.isDirectory() )
-        {
-            jmodsFolder = new File( sourceJdkModules, JMODS );
-        }
-        else
-        {
-            jmodsFolder = new File( jLinkParent, JMODS );
+        if (sourceJdkModules != null && sourceJdkModules.isDirectory()) {
+            jmodsFolder = new File(sourceJdkModules, JMODS);
+        } else {
+            jmodsFolder = new File(jLinkParent, JMODS);
         }
 
-        getLog().debug( " Parent: " + jLinkParent.getAbsolutePath() );
-        getLog().debug( " jmodsFolder: " + jmodsFolder.getAbsolutePath() );
+        getLog().debug(" Parent: " + jLinkParent.getAbsolutePath());
+        getLog().debug(" jmodsFolder: " + jmodsFolder.getAbsolutePath());
 
-        return Optional.of( jmodsFolder );
+        return Optional.of(jmodsFolder);
     }
 
-    private Commandline createJLinkCommandLine( List<String> jlinkArgs )
-    {
+    private Commandline createJLinkCommandLine(List<String> jlinkArgs) {
         Commandline cmd = new Commandline();
-        jlinkArgs.forEach( arg -> cmd.createArg().setValue( arg ) );
+        jlinkArgs.forEach(arg -> cmd.createArg().setValue(arg));
 
         return cmd;
     }
 
-    private String getJLinkExecutable()
-    {
+    private String getJLinkExecutable() {
         Optional<Toolchain> toolchain = getToolchain();
 
-        if ( !toolchain.isPresent() )
-        {
-            getLog().error( "Either JDK9+ or a toolchain "
-                    + "pointing to a JDK9+ containing a jlink binary is required." );
-            getLog().info( "See https://maven.apache.org/guides/mini/guide-using-toolchains.html "
-                    + "for mor information." );
-            throw new IllegalStateException( "Running on JDK8 and no toolchain found." );
+        if (!toolchain.isPresent()) {
+            getLog().error("Either JDK9+ or a toolchain "
+                    + "pointing to a JDK9+ containing a jlink binary is required.");
+            getLog().info("See https://maven.apache.org/guides/mini/guide-using-toolchains.html "
+                    + "for mor information.");
+            throw new IllegalStateException("Running on JDK8 and no toolchain found.");
         }
 
-        String jLinkExecutable = toolchain.orElseThrow( NoSuchElementException::new ).findTool( "jlink" );
+        String jLinkExecutable =
+                toolchain.orElseThrow(NoSuchElementException::new).findTool("jlink");
 
-        if ( jLinkExecutable.isEmpty() )
-        {
-            throw new IllegalStateException( "The jlink executable '"
-                    + jLinkExecutable + "' doesn't exist or is not a file." );
+        if (jLinkExecutable.isEmpty()) {
+            throw new IllegalStateException(
+                    "The jlink executable '" + jLinkExecutable + "' doesn't exist or is not a file.");
         }
 
         // TODO: Check if there exist a more elegant way?
-        String jLinkCommand = "jlink" + ( SystemUtils.IS_OS_WINDOWS ? ".exe" : "" );
+        String jLinkCommand = "jlink" + (SystemUtils.IS_OS_WINDOWS ? ".exe" : "");
 
-        File jLinkExe = new File( jLinkExecutable );
+        File jLinkExe = new File(jLinkExecutable);
 
-        if ( jLinkExe.isDirectory() )
-        {
-            jLinkExe = new File( jLinkExe, jLinkCommand );
+        if (jLinkExe.isDirectory()) {
+            jLinkExe = new File(jLinkExe, jLinkCommand);
         }
 
-        if ( SystemUtils.IS_OS_WINDOWS && jLinkExe.getName().indexOf( '.' ) < 0 )
-        {
-            jLinkExe = new File( jLinkExe.getPath() + ".exe" );
+        if (SystemUtils.IS_OS_WINDOWS && jLinkExe.getName().indexOf('.') < 0) {
+            jLinkExe = new File(jLinkExe.getPath() + ".exe");
         }
 
-        if ( !jLinkExe.isFile() )
-        {
-            throw new IllegalStateException( "The jlink executable '"
-                    + jLinkExe + "' doesn't exist or is not a file." );
+        if (!jLinkExe.isFile()) {
+            throw new IllegalStateException("The jlink executable '" + jLinkExe + "' doesn't exist or is not a file.");
         }
         return jLinkExe.getAbsolutePath();
     }
 
-    private int executeCommand( Commandline cmd )
-            throws MojoExecutionException
-    {
-        if ( getLog().isDebugEnabled() )
-        {
+    private int executeCommand(Commandline cmd) throws MojoExecutionException {
+        if (getLog().isDebugEnabled()) {
             // no quoted arguments ???
-            getLog().debug( CommandLineUtils.toString( cmd.getCommandline() ).replaceAll( "'", "" ) );
+            getLog().debug(CommandLineUtils.toString(cmd.getCommandline()).replaceAll("'", ""));
         }
 
         CommandLineUtils.StringStreamConsumer err = new CommandLineUtils.StringStreamConsumer();
         CommandLineUtils.StringStreamConsumer out = new CommandLineUtils.StringStreamConsumer();
-        try
-        {
-            int exitCode = CommandLineUtils.executeCommandLine( cmd, out, err );
+        try {
+            int exitCode = CommandLineUtils.executeCommandLine(cmd, out, err);
 
-            String output = StringUtils.isEmpty( out.getOutput() ) ? null : '\n' + out.getOutput().trim();
+            String output = StringUtils.isEmpty(out.getOutput())
+                    ? null
+                    : '\n' + out.getOutput().trim();
 
-            if ( exitCode != 0 )
-            {
+            if (exitCode != 0) {
 
-                if ( output != null && !output.isEmpty() )
-                {
+                if (output != null && !output.isEmpty()) {
                     // Reconsider to use WARN / ERROR ?
                     //  getLog().error( output );
-                    for ( String outputLine : output.split( "\n" ) )
-                    {
-                        getLog().error( outputLine );
+                    for (String outputLine : output.split("\n")) {
+                        getLog().error(outputLine);
                     }
                 }
 
-                StringBuilder msg = new StringBuilder( "\nExit code: " );
-                msg.append( exitCode );
-                if ( StringUtils.isNotEmpty( err.getOutput() ) )
-                {
-                    msg.append( " - " ).append( err.getOutput() );
+                StringBuilder msg = new StringBuilder("\nExit code: ");
+                msg.append(exitCode);
+                if (StringUtils.isNotEmpty(err.getOutput())) {
+                    msg.append(" - ").append(err.getOutput());
                 }
-                msg.append( '\n' );
-                msg.append( "Command line was: " ).append( cmd ).append( '\n' ).append( '\n' );
+                msg.append('\n');
+                msg.append("Command line was: ").append(cmd).append('\n').append('\n');
 
-                throw new MojoExecutionException( msg.toString() );
+                throw new MojoExecutionException(msg.toString());
             }
 
-            if ( output != null && !output.isEmpty() )
-            {
-                //getLog().info( output );
-                for ( String outputLine : output.split( "\n" ) )
-                {
-                    getLog().info( outputLine );
+            if (output != null && !output.isEmpty()) {
+                // getLog().info( output );
+                for (String outputLine : output.split("\n")) {
+                    getLog().info(outputLine);
                 }
             }
 
             return exitCode;
-        }
-        catch ( CommandLineException e )
-        {
-            throw new MojoExecutionException( "Unable to execute jlink command: " + e.getMessage(), e );
+        } catch (CommandLineException e) {
+            throw new MojoExecutionException("Unable to execute jlink command: " + e.getMessage(), e);
         }
     }
 }
--- src/test/java/org/apache/maven/plugins/jlink/AbstractJLinkMojoTest.java
+++ src/test/java/org/apache/maven/plugins/jlink/AbstractJLinkMojoTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.plugins.jlink;
 
 /*
@@ -19,10 +37,6 @@ package org.apache.maven.plugins.jlink;
  * under the License.
  */
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
@@ -33,91 +47,83 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * @author Karl Heinz Marbaise <a href="mailto:khmarbaise@apache.org">khmarbaise@apache.org</a>
  */
-public class AbstractJLinkMojoTest
-{
+public class AbstractJLinkMojoTest {
     private AbstractJLinkMojo mojoMock;
 
     @BeforeEach
-    public void before()
-    {
-        this.mojoMock = mock( AbstractJLinkMojo.class, Mockito.CALLS_REAL_METHODS );
-        when( mojoMock.getLog() ).thenReturn( mock( Log.class ) );
+    public void before() {
+        this.mojoMock = mock(AbstractJLinkMojo.class, Mockito.CALLS_REAL_METHODS);
+        when(mojoMock.getLog()).thenReturn(mock(Log.class));
     }
 
     @Test
-    @DisplayName( "convert should return single characters" )
-    public void convertShouldReturnSingleCharacter()
-    {
-        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath( "x" );
-        assertThat( result ).isNotEmpty().hasToString( "x" );
+    @DisplayName("convert should return single characters")
+    public void convertShouldReturnSingleCharacter() {
+        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath("x");
+        assertThat(result).isNotEmpty().hasToString("x");
     }
 
     @Test
-    @DisplayName( "convert should two characters separated by path separator" )
-    public void convertShouldReturnTwoCharactersSeparatedByPathSeparator()
-    {
-        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath( "x;a" );
-        assertThat( result ).hasToString( "x" + File.pathSeparatorChar + "a" );
+    @DisplayName("convert should two characters separated by path separator")
+    public void convertShouldReturnTwoCharactersSeparatedByPathSeparator() {
+        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath("x;a");
+        assertThat(result).hasToString("x" + File.pathSeparatorChar + "a");
     }
 
     @Test
-    @DisplayName( "convert using differential delimiter should return two characters separated by path separator" )
-    public void convertUsingDifferentDelimiterShouldReturnTwoCharactersSeparatedByPathSeparator()
-    {
-        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath( "x:a" );
-        assertThat( result ).hasToString( "x" + File.pathSeparatorChar + "a" );
+    @DisplayName("convert using differential delimiter should return two characters separated by path separator")
+    public void convertUsingDifferentDelimiterShouldReturnTwoCharactersSeparatedByPathSeparator() {
+        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath("x:a");
+        assertThat(result).hasToString("x" + File.pathSeparatorChar + "a");
     }
 
     @Test
-    @DisplayName( "convertSeparatedModulePathToPlatformSeparatedModulePath() "
-                          + "should return two characters separated by path separator" )
-    public void convertUsingMultipleDelimitersShouldReturnTwoCharactersSeparatedByPathSeparator()
-    {
-        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath( "x:a::" );
-        assertThat( result ).hasToString( "x" + File.pathSeparatorChar + "a" );
+    @DisplayName("convertSeparatedModulePathToPlatformSeparatedModulePath() "
+            + "should return two characters separated by path separator")
+    public void convertUsingMultipleDelimitersShouldReturnTwoCharactersSeparatedByPathSeparator() {
+        StringBuilder result = mojoMock.convertSeparatedModulePathToPlatformSeparatedModulePath("x:a::");
+        assertThat(result).hasToString("x" + File.pathSeparatorChar + "a");
     }
 
     @Test
-    @DisplayName( "getPlatformDependSeparateList() should return a single character" )
-    public void getPlatformDependSeparateListShouldReturnASingleCharacter()
-    {
-        String result = mojoMock.getPlatformDependSeparateList( Collections.singletonList( "A" ) );
-        assertThat( result ).isEqualTo( "A" );
+    @DisplayName("getPlatformDependSeparateList() should return a single character")
+    public void getPlatformDependSeparateListShouldReturnASingleCharacter() {
+        String result = mojoMock.getPlatformDependSeparateList(Collections.singletonList("A"));
+        assertThat(result).isEqualTo("A");
     }
 
     @Test
-    @DisplayName( "getPlatformDependSeparateList() should return two characters separated" )
-    public void getPlatformDependSeparateListShouldReturnTwoCharactersSeparated()
-    {
-        String result = mojoMock.getPlatformDependSeparateList( Arrays.asList( "A", "B" ) );
-        assertThat( result ).isEqualTo( "A" + File.pathSeparatorChar + "B" );
+    @DisplayName("getPlatformDependSeparateList() should return two characters separated")
+    public void getPlatformDependSeparateListShouldReturnTwoCharactersSeparated() {
+        String result = mojoMock.getPlatformDependSeparateList(Arrays.asList("A", "B"));
+        assertThat(result).isEqualTo("A" + File.pathSeparatorChar + "B");
     }
 
     @Test
-    @DisplayName( "getPlatformDependSeparateList() should return three characters separated" )
-    public void getPlatformDependSeparateListShouldReturnThreeCharactersSeparated()
-    {
-        String result = mojoMock.getPlatformDependSeparateList( Arrays.asList( "A", "B", "C" ) );
-        assertThat( result ).isEqualTo( "A" + File.pathSeparatorChar + "B" + File.pathSeparatorChar + "C" );
+    @DisplayName("getPlatformDependSeparateList() should return three characters separated")
+    public void getPlatformDependSeparateListShouldReturnThreeCharactersSeparated() {
+        String result = mojoMock.getPlatformDependSeparateList(Arrays.asList("A", "B", "C"));
+        assertThat(result).isEqualTo("A" + File.pathSeparatorChar + "B" + File.pathSeparatorChar + "C");
     }
 
     @Test
-    @DisplayName( "getCommaSeparatedList() should return a single character" )
-    public void getCommaSeparatedListShouldReturnASingleCharacter()
-    {
-        String result = mojoMock.getCommaSeparatedList( Collections.singletonList( "A" ) );
-        assertThat( result ).isEqualTo( "A" );
+    @DisplayName("getCommaSeparatedList() should return a single character")
+    public void getCommaSeparatedListShouldReturnASingleCharacter() {
+        String result = mojoMock.getCommaSeparatedList(Collections.singletonList("A"));
+        assertThat(result).isEqualTo("A");
     }
 
     @Test
-    @DisplayName( "getCommaSeparatedList() should return two characters separated by comma" )
-    public void getCommaSeparatedListShouldReturnTwoCharactersSeparatedByComma()
-    {
-        String result = mojoMock.getCommaSeparatedList( Arrays.asList( "A", "B" ) );
-        assertThat( result ).isEqualTo( "A,B" );
+    @DisplayName("getCommaSeparatedList() should return two characters separated by comma")
+    public void getCommaSeparatedListShouldReturnTwoCharactersSeparatedByComma() {
+        String result = mojoMock.getCommaSeparatedList(Arrays.asList("A", "B"));
+        assertThat(result).isEqualTo("A,B");
     }
-
 }
