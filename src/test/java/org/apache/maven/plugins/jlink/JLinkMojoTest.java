7caf7bfd272f71bd80b64d8dabe716b57b713156
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2024-12-03 17:09:03 +0000

    [MJLINK-88] separate Windows and Unix command line tests (#227)
    * split Unix and windows tests

--- src/test/java/org/apache/maven/plugins/jlink/JLinkMojoTest.java
+++ src/test/java/org/apache/maven/plugins/jlink/JLinkMojoTest.java
@@ -23,20 +23,28 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import org.apache.maven.shared.utils.cli.Commandline;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JLinkMojoTest {
 
-    @Test
-    void double_quote_every_argument() throws Exception {
+    private JLinkMojo mojo = new JLinkMojo();
+
+    @BeforeEach
+    public void setUp() throws NoSuchFieldException, IllegalAccessException {
         // given
-        JLinkMojo mojo = new JLinkMojo();
         Field stripDebug = mojo.getClass().getDeclaredField("stripDebug");
         stripDebug.setAccessible(true);
         stripDebug.set(mojo, Boolean.TRUE);
+    }
 
+    @Test
+    void double_quote_every_argument() throws Exception {
         // when
         List<String> jlinkArgs = mojo.createJlinkArgs(List.of(), List.of());
 
@@ -44,14 +52,9 @@ public class JLinkMojoTest {
         assertThat(jlinkArgs).noneMatch(arg -> arg.trim().isBlank());
     }
 
+    @DisabledOnOs(OS.WINDOWS)
     @Test
-    void single_quotes_shell_command() throws Exception {
-        // given
-        JLinkMojo mojo = new JLinkMojo();
-        Field stripDebug = mojo.getClass().getDeclaredField("stripDebug");
-        stripDebug.setAccessible(true);
-        stripDebug.set(mojo, Boolean.TRUE);
-
+    void single_quotes_shell_command_unix() throws Exception {
         // when
         List<String> jlinkArgs = mojo.createJlinkArgs(List.of("foo", "bar"), List.of("mvn", "jlink"));
         Commandline cmdLine = JLinkExecutor.createJLinkCommandLine(new File("/path/to/jlink"), jlinkArgs);
@@ -60,5 +63,19 @@ public class JLinkMojoTest {
         assertThat(cmdLine.toString())
                 .isEqualTo(
                         "/bin/sh -c '/path/to/jlink \"--strip-debug\" \"--module-path\" \"foo:bar\" \"--add-modules\" \"mvn,jlink\"'");
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void single_quotes_shell_command_windows() throws Exception {
+        // when
+        List<String> jlinkArgs = mojo.createJlinkArgs(List.of("foo", "bar"), List.of("mvn", "jlink"));
+        Commandline cmdLine = JLinkExecutor.createJLinkCommandLine(new File("/path/to/jlink"), jlinkArgs);
+
+        // then
+        assertThat(cmdLine.toString()).startsWith("cmd.exe ");
+        assertThat(cmdLine.toString())
+                .contains(
+                        "\\path\\to\\jlink \"--strip-debug\" \"--module-path\" \"foo;bar\" \"--add-modules\" \"mvn,jlink");
     }
 }
